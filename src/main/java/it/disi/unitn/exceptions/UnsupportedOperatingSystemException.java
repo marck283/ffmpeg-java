cd1389c1e0944940eaf7b2bc0f118e8117414405
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2024-03-09 15:30:09 +0100

    Modified JavaDoc

--- src/main/java/it/disi/unitn/exceptions/UnsupportedOperatingSystemException.java
+++ src/main/java/it/disi/unitn/exceptions/UnsupportedOperatingSystemException.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 
 /**
  * This exception is thrown when the operating system on which the user is operating is not supported. Currently
- * supported Operating Systems include only Windows and Linux (e.g., Kali Linux, Ubuntu) operating systems.
+ * supported Operating Systems include only Windows and Linux (e.g., Kali Linux, Ubuntu).
  */
 public class UnsupportedOperatingSystemException extends Exception {
     /**
