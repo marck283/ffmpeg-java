fb2c518501239bbc9be196cce9b3c59dde07a728
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2024-03-28 07:58:47 +0100

    Added filter type

---
+++ src/main/java/it/disi/unitn/videocreator/filtergraph/FilterGraph.java
@@ -0,0 +1,32 @@
+package it.disi.unitn.videocreator.filtergraph;
+
+import it.disi.unitn.videocreator.filtergraph.filterchain.FilterChain;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilterGraph {
+
+    private final List<FilterChain> fcList;
+
+    public FilterGraph() {
+        fcList = new ArrayList<>();
+    }
+
+    public void addFilterChain(@NotNull FilterChain filterChain) {
+        fcList.add(filterChain);
+    }
+
+    public void removeFilterChain(@NotNull FilterChain filterChain) {
+        fcList.remove(filterChain);
+    }
+
+    public String toString() {
+        List<String> helperList = new ArrayList<>();
+        for(FilterChain fc: fcList) {
+            helperList.add(fc.toString());
+        }
+        return String.join(";", helperList);
+    }
+}
