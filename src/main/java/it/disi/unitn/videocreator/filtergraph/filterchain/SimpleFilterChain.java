63d33006ca09ca60a81adf1ed33babbc19514a25
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2024-03-29 16:39:37 +0100

    Fixed filter chains

--- src/main/java/it/disi/unitn/videocreator/filtergraph/filterchain/SimpleFilterChain.java
+++ src/main/java/it/disi/unitn/videocreator/filtergraph/filterchain/SimpleFilterChain.java
@@ -7,7 +7,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SimpleFilterChain {
+public abstract class SimpleFilterChain {
 
     protected final List<Filter> filterList;
 
@@ -20,7 +20,7 @@ public class SimpleFilterChain {
      * @param filter The given Filter instance
      * @throws InvalidArgumentException If the given Filter instance is null
      */
-    private void checkNullFilter(@NotNull Filter filter) throws InvalidArgumentException {
+    protected void checkNullFilter(@NotNull Filter filter) throws InvalidArgumentException {
         if(filter == null) {
             throw new InvalidArgumentException("No new Filter instance can be null.", "Nessuna nuova istanza di Filter puo' " +
                     "essere null.");
@@ -32,20 +32,14 @@ public class SimpleFilterChain {
      * @param filter The Filter to be added
      * @throws InvalidArgumentException If the given argument is null
      */
-    public void addFilter(@NotNull Filter filter) throws InvalidArgumentException {
-        checkNullFilter(filter);
-        filterList.add(filter);
-    }
+    public abstract void addFilter(@NotNull Filter filter) throws InvalidArgumentException;
 
     /**
      * Removes the given Filter from this filter chain
      * @param filter The Filter to be removed
      * @throws InvalidArgumentException If the given argument is null
      */
-    public void removeFilter(@NotNull Filter filter) throws InvalidArgumentException {
-        checkNullFilter(filter);
-        filterList.remove(filter);
-    }
+    public abstract void removeFilter(@NotNull Filter filter) throws InvalidArgumentException;
 
     /**
      * This method returns the String representation of this filter chain.
