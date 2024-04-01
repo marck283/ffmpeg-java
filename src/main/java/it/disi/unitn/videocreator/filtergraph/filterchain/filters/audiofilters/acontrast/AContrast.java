d131c166533d26da0c70e8f97cb1afd811bd3c42
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2024-04-02 01:13:38 +0200

    Added two audio filters

---
+++ src/main/java/it/disi/unitn/videocreator/filtergraph/filterchain/filters/audiofilters/acontrast/AContrast.java
@@ -0,0 +1,40 @@
+package it.disi.unitn.videocreator.filtergraph.filterchain.filters.audiofilters.acontrast;
+
+import it.disi.unitn.exceptions.InvalidArgumentException;
+import it.disi.unitn.videocreator.filtergraph.filterchain.filters.Filter;
+
+/**
+ * Simple audio dynamic range compression/expansion filter.
+ */
+public class AContrast extends Filter {
+
+    private int contrast = 33;
+
+    /**
+     * This class's constructor. Constructs a new filter (whether video or audio).
+     *
+     * @throws InvalidArgumentException If the given filter's name is null or an empty string
+     */
+    protected AContrast() throws InvalidArgumentException {
+        super("acontrast");
+    }
+
+    /**
+     * Sets the contrast.
+     * @param val The new contrast value
+     * @throws InvalidArgumentException If the new contrast value is negative or greater than 100
+     */
+    public void setContrast(int val) throws InvalidArgumentException {
+        if(val < 0 || val > 100) {
+            throw new InvalidArgumentException("The contrast value cannot be negative or greater than 100.", "Il valore " +
+                    "del contrasto non puo' essere negativo o maggiore di 100.");
+        }
+
+        contrast = val;
+    }
+
+    @Override
+    protected void updateMap() {
+        options.put("contrast", String.valueOf(contrast));
+    }
+}
