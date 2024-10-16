995af6273fd38d1b02af32ba8be235da13e0def5
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2024-10-16 22:22:27 +0200

    Fixed deprecated features warning

--- build/scripts/ffmpeg-java-id.bat
+++ build/scripts/ffmpeg-java-id.bat
@@ -13,6 +13,8 @@
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
 @rem
+@rem SPDX-License-Identifier: Apache-2.0
+@rem
 
 @if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
@@ -68,7 +70,7 @@ goto fail
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\ffmpeg-java-id-1.2.1.jar;%APP_HOME%\lib\annotations-24.1.0.jar;%APP_HOME%\lib\gson-2.11.0.jar;%APP_HOME%\lib\slf4j-api-2.0.13.jar;%APP_HOME%\lib\commons-io-2.16.1.jar;%APP_HOME%\lib\commons-lang3-3.14.0.jar;%APP_HOME%\lib\commons-exec-1.4.0.jar;%APP_HOME%\lib\encoder-1.2.3.jar;%APP_HOME%\lib\error_prone_annotations-2.27.0.jar
+set CLASSPATH=%APP_HOME%\lib\ffmpeg-java-id-1.2.9-beta2.jar;%APP_HOME%\lib\annotations-26.0.1.jar;%APP_HOME%\lib\google-cloud-texttospeech-2.53.0.jar;%APP_HOME%\lib\gson-2.11.0.jar;%APP_HOME%\lib\slf4j-api-2.0.16.jar;%APP_HOME%\lib\commons-io-2.17.0.jar;%APP_HOME%\lib\commons-lang3-3.17.0.jar;%APP_HOME%\lib\commons-exec-1.4.0.jar;%APP_HOME%\lib\encoder-1.3.1.jar;%APP_HOME%\lib\spotbugs-annotations-4.8.6.jar;%APP_HOME%\lib\auto-value-annotations-1.11.0.jar;%APP_HOME%\lib\google-auth-library-credentials-1.28.0.jar;%APP_HOME%\lib\google-auth-library-oauth2-http-1.28.0.jar;%APP_HOME%\lib\google-http-client-1.45.0.jar;%APP_HOME%\lib\google-http-client-gson-1.45.0.jar;%APP_HOME%\lib\guava-33.3.1-jre.jar;%APP_HOME%\lib\protobuf-java-3.25.5.jar;%APP_HOME%\lib\protobuf-java-util-3.25.5.jar;%APP_HOME%\lib\grpc-alts-1.67.1.jar;%APP_HOME%\lib\grpc-api-1.67.1.jar;%APP_HOME%\lib\grpc-auth-1.67.1.jar;%APP_HOME%\lib\grpc-context-1.67.1.jar;%APP_HOME%\lib\grpc-core-1.67.1.jar;%APP_HOME%\lib\grpc-googleapis-1.67.1.jar;%APP_HOME%\lib\grpc-grpclb-1.67.1.jar;%APP_HOME%\lib\grpc-inprocess-1.67.1.jar;%APP_HOME%\lib\grpc-netty-shaded-1.67.1.jar;%APP_HOME%\lib\grpc-protobuf-1.67.1.jar;%APP_HOME%\lib\grpc-protobuf-lite-1.67.1.jar;%APP_HOME%\lib\grpc-services-1.67.1.jar;%APP_HOME%\lib\grpc-stub-1.67.1.jar;%APP_HOME%\lib\grpc-util-1.67.1.jar;%APP_HOME%\lib\grpc-xds-1.67.1.jar;%APP_HOME%\lib\gax-2.55.0.jar;%APP_HOME%\lib\gax-grpc-2.55.0.jar;%APP_HOME%\lib\gax-httpjson-2.55.0.jar;%APP_HOME%\lib\api-common-2.38.0.jar;%APP_HOME%\lib\proto-google-common-protos-2.46.0.jar;%APP_HOME%\lib\proto-google-cloud-texttospeech-v1-2.53.0.jar;%APP_HOME%\lib\proto-google-cloud-texttospeech-v1beta1-0.142.0.jar;%APP_HOME%\lib\error_prone_annotations-2.33.0.jar;%APP_HOME%\lib\jsr305-3.0.2.jar;%APP_HOME%\lib\javax.annotation-api-1.3.2.jar;%APP_HOME%\lib\j2objc-annotations-3.0.0.jar;%APP_HOME%\lib\failureaccess-1.0.2.jar;%APP_HOME%\lib\listenablefuture-9999.0-empty-to-avoid-conflict-with-guava.jar;%APP_HOME%\lib\checker-qual-3.47.0.jar;%APP_HOME%\lib\opencensus-api-0.31.1.jar;%APP_HOME%\lib\annotations-4.1.1.4.jar;%APP_HOME%\lib\animal-sniffer-annotations-1.24.jar;%APP_HOME%\lib\conscrypt-openjdk-uber-2.5.2.jar;%APP_HOME%\lib\perfmark-api-0.27.0.jar;%APP_HOME%\lib\re2j-1.7.jar;%APP_HOME%\lib\httpclient-4.5.14.jar;%APP_HOME%\lib\commons-codec-1.17.1.jar;%APP_HOME%\lib\httpcore-4.4.16.jar;%APP_HOME%\lib\opencensus-contrib-http-util-0.31.1.jar;%APP_HOME%\lib\threetenbp-1.7.0.jar
 
 
 @rem Execute ffmpeg-java-id
