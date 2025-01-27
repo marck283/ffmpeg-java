d8398fece98932c810beb3298dac01137f2a771b
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2025-01-27 11:07:51 +0100

    Added VideoExtractor class

--- src/main/java/it/disi/unitn/videocreator/VideoExtractor.java
+++ src/main/java/it/disi/unitn/videocreator/VideoExtractor.java
@@ -1,14 +1,11 @@
-package it.disi.unitn.lasagna.audiocreator;
+package it.disi.unitn.videocreator;
 
 import it.disi.unitn.FFMpegBuilder;
 import it.disi.unitn.StringExt;
 import it.disi.unitn.exceptions.InvalidArgumentException;
 import org.jetbrains.annotations.NotNull;
 
-/**
- * This class implements an audio extractor.
- */
-public class AudioExtractor {
+public class VideoExtractor {
 
     private final String inputFile, outputFile;
 
@@ -21,11 +18,11 @@ public class AudioExtractor {
      * @param outputFile The output file path. This value cannot be null or an empty string.
      * @param inputFile  The input file path. This value cannot be null or an empty string.
      */
-    public AudioExtractor(@NotNull FFMpegBuilder builder, @NotNull String outputFile, @NotNull String inputFile)
+    public VideoExtractor(@NotNull FFMpegBuilder builder, @NotNull String outputFile, @NotNull String inputFile)
             throws InvalidArgumentException {
         if(builder == null || StringExt.checkNullOrEmpty(outputFile) || StringExt.checkNullOrEmpty(inputFile)) {
-            throw new InvalidArgumentException("None of the arguments given to AudioExtractor's constructor can be null " +
-                    "or an empty string.", "Nessuno degli argomenti forniti al costruttore di AudioExtractor puo' essere " +
+            throw new InvalidArgumentException("None of the arguments given to VideoExtractor's constructor can be null " +
+                    "or an empty string.", "Nessuno degli argomenti forniti al costruttore di VideoExtractor puo' essere " +
                     "null o una stringa vuota.");
         }
         this.ffmpegBuilder = builder;
@@ -34,16 +31,17 @@ public class AudioExtractor {
     }
 
     /**
-     * This method creates the command that will be run to extract the audio track from the input file.
+     * This method creates the command that will be run to extract the video track from the input file.
      */
     public void createCommand() {
         try {
             ffmpegBuilder.addInput(inputFile);
-            ffmpegBuilder.add("-vn");
+            ffmpegBuilder.add("-an");
             ffmpegBuilder.addOutput(outputFile);
         } catch(InvalidArgumentException e) {
             System.err.println(e.getMessage());
             throw new RuntimeException();
         }
     }
+
 }
---
+++ src/test/java/it/disi/unitn/videocreator/VideoExtractorTest.java
@@ -0,0 +1,23 @@
+package it.disi.unitn.videocreator;
+
+import it.disi.unitn.FFMpeg;
+import it.disi.unitn.FFMpegBuilder;
+import it.disi.unitn.exceptions.InvalidArgumentException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class VideoExtractorTest {
+
+    @Test
+    public void extractVideo() throws InvalidArgumentException, IOException {
+        FFMpegBuilder builder = new FFMpegBuilder("ffmpeg");
+        VideoExtractor extractor = new VideoExtractor(builder, "./src/test/resources/output/mp4/000.wmv",
+                "./src/test/resources/input/mp4/000.wmv");
+        extractor.createCommand();
+        FFMpeg ffmpeg = builder.build();
+        ffmpeg.executeCMD(1L, TimeUnit.MINUTES, "./", null);
+    }
+
+}
